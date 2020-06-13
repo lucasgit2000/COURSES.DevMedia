bbd2b0d5c6233b01c73399cb912db38e5d862b9c
mrrobot <dayyumn@gmail.com>
2020-06-12 21:46:05 -0300

    inicio curso unity 3d criacao de games, projeto iniciado, assets e cenas iniciais.

---
+++ Unity3D/ItroducaoAoUnityDesenvolvimentoGames/Assets/Movimento.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizontal, vertical));
+    }
+}
---
+++ Unity3D/ItroducaoAoUnityDesenvolvimentoGames/Assets/MovimentoEnquantoApertaBotao.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoEnquantoApertaBotao : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.right);
+        }
+        
+    }
+}
