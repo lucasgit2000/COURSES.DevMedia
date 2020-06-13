543939894428e768e05515392379bea7fcb719dc
mrrobot <dayyumn@gmail.com>
2020-06-13 12:47:48 -0300

    creating objects tags and printing them with script.

---
+++ Unity3D/ItroducaoAoUnityDesenvolvimentoGames/Assets/GetGameObjectsByTagName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetGameObjectsByTagName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Cube");
+
+        foreach (var o in objects)
+        {
+            print(o);
+        }
+    }
+}
